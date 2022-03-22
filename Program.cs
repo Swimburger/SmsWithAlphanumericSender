2c72a5724b7a1bb55b17bab8004becd1def03776
Niels Swimberghe <3382717+Swimburger@users.noreply.github.com>
2022-03-22 19:36:14 -0400

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Twilio;
+using Twilio.Http;
+using Twilio.Rest.Api.V2010.Account;
+using Twilio.Types;
+
+// build configuration
+using var host = Host.CreateDefaultBuilder(args).Build();
+var config = host.Services.GetRequiredService<IConfiguration>();
+
+// grab configuration
+var useMessagingService = config.GetValue<bool>("UseMessagingService");
+var alphanumericSender = config["AlphanumericSender"];
+var messagingServiceSid = config["TwilioMessagingServiceSid"];
+var accountSid = config["TwilioAccountSid"];
+var authToken = config["TwilioAuthToken"];
+
+// ask user for phone number and message to text
+Console.Write("Who do you want to text? ");
+var toPhoneNumber = Console.ReadLine();
+Console.WriteLine();
+
+Console.Write("What is your message? ");
+var messageBody = Console.ReadLine();
+Console.WriteLine();
+
+// authenticate using the TwilioClient
+TwilioClient.Init(accountSid, authToken);
+
+if (useMessagingService)
+{
+    // send text message using a Messaging Service
+    MessageResource.Create(
+        to: new PhoneNumber(toPhoneNumber),
+        messagingServiceSid: messagingServiceSid,
+        body: messageBody
+    );
+}
+else
+{
+    // send text message without a Messaging Service
+    var request = new Request(Twilio.Http.HttpMethod.Post, $"https://api.twilio.com/2010-04-01/Accounts/{accountSid}/Messages.json");
+    request.AddPostParam("Body",messageBody);
+    request.AddPostParam("To",toPhoneNumber);
+    request.AddPostParam("From",alphanumericSender);
+    TwilioClient.GetRestClient().Request(request);
+}
---
+++ readme.md
@@ -0,0 +1,19 @@
+# Send SMS with an Alpha Numeric Sender
+
+To run this sample:
+1. Configure the project using user-secrets
+```bash
+dotnet user-secrets set TwilioAccountSid <YourAccountSid>
+dotnet user-secrets set TwilioAuthToken <YourAuthToken>
+
+# if you want to use a Messaging Service:
+dotnet user-secrets set TwilioMessagingServiceSid <YourMessagingServiceSid>
+
+# if you don't want to use a Messaging Service:
+dotnet user-secrets set AlphanumericSender <YourAlphanumericSender>
+```
+
+2. Run the project with the following arguments:
+```bash
+dotnet run --environment=Development --UseMessagingService=<Replace with True/False>
+```
